2a0d39dd844853b3a960abbc0fd1f52589e84421
jillou <jilljill860524@gmail.com>
2020-07-27 11:56:16 +0800

    Initial Commit

---
+++ Avatar_Choose/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Avatar_Choose
+//
+//  Created by JillOU on 2020/7/27.
+//  Copyright © 2020 Jillou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
